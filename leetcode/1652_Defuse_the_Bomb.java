78b995eadc8d3341b62b570f00309b353a6b4907
Pragya <pragya.bachhawat23@lpu.in>
2026-06-25 11:46:45 +0530

    Create 1652_Defuse_the_Bomb.java

---
+++ leetcode/1652_Defuse_the_Bomb.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int n = code.length;
+            int[] arr=new int[n];
+            if(k==0){
+                return arr;
+            }
+        for(int i=0;i<n;i++){
+            int sum=0;
+            if(k>0){
+                for(int j=1;j<=k;j++){
+                    sum+=code[(i+j)%n];
+                }
+            }
+            else{
+                for(int j=1;j<=-k;j++){
+                sum+=code[(i-j+n)%n];
+                }
+            }
+            arr[i]=sum;
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        Solution s=new Solution();
+        int[] code={5,7,1,4};
+        int k=3;
+        int[] ans=s.decrypt(code,k);
+        System.out.println(Arrays.toString(ans));
+    }
+}
