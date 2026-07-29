9b8bc3c8308def0aa6456f8abae96a9875346faa
Pragya <pragyabachhawat71@gmail.com>
2026-07-29 16:40:03 +0530

    Create 204_Count_Primes.cpp

---
+++ leetcode/204_Count_Primes.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<bool> prime(n,true);
+        if(n>0)prime[0]=false;
+        if(n>1)prime[1]=false;
+        for(int i=2;i*i<n;i++){
+            if(prime[i]){
+                for(int j=i*i;j<n;j+=i){
+                    prime[j]=false;
+                }
+            }
+        }
+        int cnt=0;
+
+        for(int i=2;i<n;i++){
+            if(prime[i])cnt++;
+        }
+        return cnt;
+    }
+};
+
+int main(){
+    Solution s;
+    int n=10;
+    cout<<s.countPrimes(n);
+    return 0;
+}
