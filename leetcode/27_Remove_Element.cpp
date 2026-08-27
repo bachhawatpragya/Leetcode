9a4b7d40a9688d6925258739ad72957151f4fcfe
Pragya <pragyabachhawat71@gmail.com>
2026-08-28 00:54:21 +0530

    Create 27_Remove_Element.cpp

---
+++ leetcode/27_Remove_Element.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=val){
+                nums[k++]=nums[i];
+            }
+        }
+        return k;
+    }
+};
+int main(){
+    Solution s;
+    vector<int> nums={3,2,2,3};
+    int val=3;
+    int k=s.removeElement(nums,val);
+    cout<<k<<endl;
+    for(int i=0;i<k;i++){
+        cout<<nums[i]<<" ";
+    }
+}
