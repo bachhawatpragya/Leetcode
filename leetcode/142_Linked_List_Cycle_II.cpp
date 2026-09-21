140523acb0c3e9bc934008913555516d9a327d07
Pragya <pragyabachhawat71@gmail.com>
2026-09-21 15:34:03 +0530

    Create 142_Linked_List_Cycle_II.cpp

---
+++ leetcode/142_Linked_List_Cycle_II.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+//   Definition for singly-linked list.
+  struct ListNode {
+      int val;
+     ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+  };
+ 
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                slow=head;
+                while(slow!=fast){
+                    slow=slow->next;
+                    fast=fast->next;
+                }
+                return slow;
+            }
+        }
+        return nullptr;
+    }
+};
+
+int main() {
+    Solution s;
+    ListNode* head=new ListNode(3);
+    head->next=new ListNode(2);
+    head->next->next=new ListNode(0);
+    head->next->next->next=new ListNode(-4);
+    head->next->next->next->next=head->next; // Create a cycle
+    ListNode* cycleNode=s.detectCycle(head);
+    if(cycleNode){
+        cout<<"Cycle detected at node with value: "<<cycleNode->val<<endl;
+    } else {
+        cout<<"No cycle detected."<<endl;
+    }
+    return 0;
+}
